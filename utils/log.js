f07316151a3d5d382100057d2f794933d1bd1492
scar17off <scar17off@gmail.com>
2023-12-15 20:25:18 +0200

    logging levels

--- utils/log.js
+++ utils/log.js
@@ -1,18 +1,15 @@
 const chalk = require('chalk');
 
-if(false) {
-    chalk.blue = chalk.blueBright;
-    chalk.red = chalk.redBright;
-    chalk.green = chalk.greenBright;
-    chalk.yellow = chalk.yellowBright;
-    chalk.magenta = chalk.magentaBright;
+const LEVELS = {
+    "INFO": "blue",
+    "WARN": "orange"
 };
 
 Date.prototype.today = function () { 
     return ((this.getDate() < 10)?"0":"") + this.getDate() +"-"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"-"+ this.getFullYear();
 };
 Date.prototype.timeNow = function () {
-     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+    return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 };
 
 function log(...message) {
@@ -20,9 +17,9 @@ function log(...message) {
     message.pop();
     message = message.join(" ");
 
-	let dt = new Date();
+    const color = LEVELS[type];
 
-    msg = chalk.blue('[' + dt.timeNow() + ` ${type}]: `) + chalk.white(message);
+    msg = chalk[color]('[' + new Date().timeNow() + ` ${type}]: `) + chalk.white(message);
 
 	console.log(msg);
 };
