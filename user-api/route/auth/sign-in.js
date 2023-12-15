659a3d8aaf8dcb8267ab979c5be519e250de37d0
scar17off <scar17off@gmail.com>
2023-12-16 01:34:19 +0200

    401 404

--- user-api/route/auth/sign-in.js
+++ user-api/route/auth/sign-in.js
@@ -3,9 +3,10 @@ const { queue_search } = require("../../../utils/users.js");
 module.exports = (req, res) => {
     const user = queue_search(req.body.email, "email");
 
-    if(typeof user == "undefined" ||
-        req.body.password !== user.password
-    ) {
+    if(!user) {
+        res.sendStatus(404);
+        return;
+    } else if(user.password !== req.body.password) {
         res.sendStatus(401);
         return;
     };
