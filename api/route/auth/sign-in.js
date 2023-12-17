572a5d7ad417cc7dc69cb02da61c710f9d5bfa29
scar17off <scar17off@gmail.com>
2023-12-18 01:19:59 +0200

    i forgot x2

--- api/route/auth/sign-in.js
+++ api/route/auth/sign-in.js
@@ -2,7 +2,6 @@ const { queue_search } = require("../../../components/User.js");
 
 module.exports = (req, res) => {
     const user = queue_search(req.body.email, "email");
-    console.log(req.body);
 
     if(!user) {
         res.sendStatus(404);
