925037693f262b7e1b7cf40067abe48135ad24b0
scar17off <scar17off@gmail.com>
2024-02-17 21:47:06 +0200

    newMessage fix

--- api/route/user/send-message.js
+++ api/route/user/send-message.js
@@ -1,17 +1,19 @@
-const { User, safe_user } = require("../../../components/User.js");
+const { User, query_search } = require("../../../components/User.js");
 const Message = require("../../../components/Message.js");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     if(!req.cookies.token) {
         res.sendStatus(401);
         return;
     }
 
-    const author = new User({ token: req.cookies.token });
-
-    const message = new Message(safe_user(author), req.body.content);
-
+    const author = await new User().initWithToken(req.cookies.token);
+    const receiverProperties = await query_search(req.body.receiver, "id");
+    const receiver = await new User().initWithToken(receiverProperties.token);
+    const message = new Message(author.id, receiver.id, req.body.content);
     message.save();
 
-    res.status(200);
+    receiver.send("newMessage", { from: author.id, content: req.body.content });
+
+    res.sendStatus(200);
 }
