cdb0f89ab2207817b936b75928bb9b2f7e870d5e
scar17off <scar17off@gmail.com>
2024-05-10 23:42:20 +0300

    Unnecessary code line

--- utils/get_ip.js
+++ utils/get_ip.js
@@ -1,3 +1,5 @@
 module.exports = req => {
     return (req.headers['x-forwarded-for'] || req.connection.remoteAddress).split(",")[0].replace('::ffff:', '');
 }
+
+// This may not typically be utilized in a coding environment.
