d76297c2c408459b60d7e381dff2ad4c3943fe40
scar17off <scar17off@gmail.com>
2024-08-11 15:23:43 +0300

    Decreased frequency of warnings in the console

--- frontend/src/pages/mainpage/Components/Avatar.js
+++ frontend/src/pages/mainpage/Components/Avatar.js
@@ -2,22 +2,22 @@ import React from 'react';
 import './Avatar.css';
 
 const Avatar = ({ pfpURL, status, width = "64", height = "64" }) => {
-  const statusSize = parseInt(width) / 4;
-  return (
-    <div className="avatar-wrapper">
-      <img src={pfpURL} className="avatar" width={width} height={height} />
-      {status && (
-        <div
-          className="status-indicator"
-          style={{
-            backgroundColor: `var(--${status}-status)`,
-            width: `${statusSize}px`,
-            height: `${statusSize}px`
-          }}
-        ></div>
-      )}
-    </div>
-  );
+	const statusSize = parseInt(width) / 4;
+	return (
+		<div className="avatar-wrapper">
+			<img src={pfpURL} className="avatar" width={width} height={height} alt="User avatar" />
+			{status && (
+				<div
+					className="status-indicator"
+					style={{
+						backgroundColor: `var(--${status}-status)`,
+						width: `${statusSize}px`,
+						height: `${statusSize}px`
+					}}
+				></div>
+			)}
+		</div>
+	);
 };
 
 export default Avatar;
--- frontend/src/pages/mainpage/events.js
+++ frontend/src/pages/mainpage/events.js
@@ -1,2 +1,3 @@
 import EventEmitter from 'eventemitter3';
-export default new EventEmitter();
+const eventEmitter = new EventEmitter();
+export default eventEmitter;
